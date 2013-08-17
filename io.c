766c94841225b86d6eaee41d1c4df4dd7a0cc72f
Sam Russell <sam.h.russell@gmail.com>
2013-08-17 14:11:12 +1200

    created files to manage file/stream access

---
+++ io.c
@@ -0,0 +1,8 @@
+/*
+ * IO.C
+ * This will interface with files and streams, and pass frames to/from other modules
+ */
+
+#include "io.h"
+
+
---
+++ io.h
@@ -0,0 +1,8 @@
+/*
+ * IO.H
+ * This will interface with files and streams, and pass frames to/from other modules
+ */
+
+// start
+
+
